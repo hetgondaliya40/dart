9f1449e3def5ae61ac33bcee0482e63fb039b70e
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-04-18 10:44:15 +0530

    Update 2.dart

--- dart/pr_1/2.dart
+++ dart/pr_1/2.dart
@@ -17,22 +17,22 @@ The largest number is: 99
 import 'dart:io';
 
 void main() {
-  List<int> allNumbers = [];
+  List<int> allNum = [];
 
   print("Enter any 10 numbers: ");
 
   for (int i = 0; i < 10; i++) {
     String userInput = stdin.readLineSync()!;
     int number = int.parse(userInput);
-    allNumbers.add(number);
+    allNum.add(number);
   }
 
-  int largestNumber = allNumbers[0];
-  for (int i = 1; i < allNumbers.length; i++) {
-    if (allNumbers[i] > largestNumber) {
-      largestNumber = allNumbers[i];
+  int largestNum = allNum[0];
+  for (int i = 1; i < allNum.length; i++) {
+    if (allNum[i] > largestNum) {
+      largestNum = allNum[i];
     }
   }
 
-  print('\nThe largest number is: $largestNumber');
+  print('\nThe largest number is: $largestNum');
 }
