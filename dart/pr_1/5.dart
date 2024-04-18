20357029b098935abcabcaa6c51418d796a97c13
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-04-18 21:39:54 +0530

    Update 5.dart

--- dart/pr_1/5.dart
+++ dart/pr_1/5.dart
@@ -5,96 +5,108 @@ Perform below all mentioned by user choice:
 import 'dart:io';
 
 void main() {
-  int choice, x, y, sum;
-
-  print("press 1 for sum of all elements..");
-  print("press 2 for rowsum..");
-  print("press 3 for colom sum..");
-  print("press 4 for daigonal..");
-  print("press 5 for anti daigonal..");
-  print("press 6 for exit progaram..");
-
-  stdout.write("Enter your choice :- ");
-  choice = int.parse(stdin.readLineSync()!);
-
-  List a = [
+  List<List<int>> a = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
   ];
-  List b = [
-    [3, 3, 3],
-    [3, 3, 3],
-    [3, 3, 3]
-  ];
-  List c = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ];
-  switch (choice) {
-    case 1:
-      for (int i = 0; i <= 2; i++) {
-        for (int j = 0; j <= 2; j++) {
-          c[i][j] = a[i][j] + b[i][j];
-        }
-      }
+  int choice;
 
-      print("$c");
-      break;
-    case 2:
-      stdout.write("Enter your  row number ");
-      x = int.parse(stdin.readLineSync()!);
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (i == x) {
-            sum = a[x][j] + b[x][j];
-            print("$sum");
-          }
-        }
-      }
-      break;
-
-    case 3:
-      stdout.write("Enter your  colom number ");
-      y = int.parse(stdin.readLineSync()!);
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (i == y) {
-            sum = a[i][y] + b[i][y];
-            print("$sum");
-          }
-        }
-      }
-      break;
-
-    case 4:
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (i == j) {
-            sum = a[i][j] + b[i][j];
-            print("$sum");
-          }
-        }
-      }
-      break;
-
-    case 5:
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (i + j == a[i][j] - 1) {
-            sum = a[i][j] + b[i][j];
-            print("$sum");
-          }
-        }
-      }
-      break;
-
-    case 6:
-      stdout.write("data finish......");
-      break;
-    default:
-      stdout.write("invlid input....");
-      break;
+  print("\n----:Array a:----\n");
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      stdout.write('Enter element of a[$i][$j] : ');
+      int val = int.parse(stdin.readLineSync()!);
+      a[i][j] = val;
+    }
   }
+
+  do {
+    print("\n1. Sum of all elements");
+    print("2. Sum of specific row");
+    print("3. Sum of specific column");
+    print("4. Sum of diagonal elements");
+    print("5. Sum of antidiagonal elements");
+    print("0. Exit");
+    stdout.write("Enter your choice: ");
+    choice = int.parse(stdin.readLineSync()!);
+
+    switch (choice) {
+      case 1:
+        choice = 0;
+        for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) choice += a[i][j];
+        }
+
+        print("\n========================");
+        print("Sum of all elements : ${choice++}");
+        print("========================");
+        break;
+
+      case 2:
+        choice = 0;
+        stdout.write("\nEnter row : ");
+        int r = int.parse(stdin.readLineSync()!);
+
+        for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) {
+            if (i == r) choice += a[i][j];
+          }
+        }
+
+        print("\n==================");
+        print("Sum of $r row : ${choice++}");
+        print("==================");
+        break;
+
+      case 3:
+        choice = 0;
+        stdout.write("\nEnter row : ");
+        int c = int.parse(stdin.readLineSync()!);
+
+        for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) {
+            if (j == c) choice += a[i][j];
+          }
+        }
+
+        print("\n=====================");
+        print("Sum of $c colloum : ${choice++}");
+        print("=====================");
+        break;
+
+      case 4:
+        choice = 0;
+        for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) {
+            if (i == j) choice += a[i][j];
+          }
+        }
+        print("\n===========================");
+        print("Sum of diagonal element : ${choice++}");
+        print("===========================");
+        break;
+
+      case 5:
+        choice = 0;
+        for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) {
+            if (i + j == a.length - 1) choice += a[i][j];
+          }
+        }
+
+        print("\n==============================");
+        print("Sum of antidiagonal element : ${choice++}");
+        print("==============================");
+        break;
+
+      case 0:
+        print("\n\nExit !!");
+        break;
+
+      default:
+        print("\n\nInvalid input !!");
+        break;
+    }
+  } while (choice != 0);
 }
