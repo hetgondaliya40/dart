ffbe14fa42d905c9c5d52246ebbea7266ccf86e7
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-04-25 11:45:14 +0530

    Update 1.dart

--- dart/pr_1/1.dart
+++ dart/pr_1/1.dart
@@ -1,22 +1,27 @@
 /*Write a Dart program to to print all negative elements in an array.
 
  */
-void main() {
-    List<int> myArray = [];
-    stdout.write("How many elements do you want in your Array : ");
-    int len = int.parse(stdin.readLineSync()!);
-    myArray = List.generate(len, (i) {
+ import 'dart:io';
+void main(){
+  List myArray = [];
+  stdout.write("How many elements do you want in your Array : ");
+  int len = int.parse(stdin.readLineSync()!);
+  myArray = List.generate(len, (i) {
       stdout.write("Enter the value for Element ${i+1} : ");
       int el = int.parse(stdin.readLineSync()!);
       myArray.add(el);
       return el;
     });
-    int large = myArray[0];
-    myArray.forEach((element) {
-      if(element>large){
-        large = element;
-      }
+  print("Elements in the Array are : ");
+  myArray.forEach((element) {
+      stdout.write("$element   ");
+  });
+  print("");
+  print("Negative Elements in the Array are : ");
+  myArray.forEach((element) {
+    if(element<0){
+      stdout.write("$element   ");
     }
-                   );
-    print("The Largest Element in the Array is : $large");
+  }
+);
 }
